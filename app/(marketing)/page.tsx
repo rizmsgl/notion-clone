d5d41851150bb638f5ede72e705a4be425369541
ghaith_mlika <mlika.gaith@gmail.com>
2024-01-10 21:06:18 +0100

    Add footer

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import {Hero} from "@/app/(marketing)/_components/Hero";
+import {Footer} from "@/app/(marketing)/_components/Footer";
 
 
 const MarketingPage = ()=>{
     return(
         <div className="min-h-full dark:bg-darkBackground">
             <Hero/>
+            <Footer/>
         </div>
     )
 }

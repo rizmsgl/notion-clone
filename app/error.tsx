6575f3829f3d1724d8d4134c460c358d2489bd7c
ghaith <mlika.gaith@gmail.com>
2024-06-10 21:24:25 +0100

    Add custom error and not found pages

---
+++ app/error.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+const Error = () => {
+  return (
+    <div className='h-full flex flex-col items-center justify-center space-y-4'>
+      <Image src="/illustrations/error.png" height="300" width="300" alt="Error" className='dark:hidden'/>
+      <Image src="/illustrations/error-dark.png" height="300" width="300" alt="Error" className='hidden dark:block'/>
+      <h2 className='font-medium text-xl'>Something Went Wrong</h2>
+      <Button asChild>
+        <Link href="/">Go Back to Note Niche</Link>
+      </Button>
+    </div>
+  )
+}
+
+export default Error
---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const NotFound = () => {
+  return (
+    <div className='h-full flex flex-col items-center justify-center space-y-4'>
+      <Image src="/illustrations/not-found.png" height="300" width="300" alt="Not Found" className='dark:hidden'/>
+      <Image src="/illustrations/not-found-dark.png" height="300" width="300" alt="Not Found" className='hidden dark:block'/>
+      <h2 className='font-medium text-xl'>Nothing Here</h2>
+      <Button asChild>
+        <Link href="/">Go Back to Note Niche</Link>
+      </Button>
+    </div>
+  )
+}
+
+export default NotFound
